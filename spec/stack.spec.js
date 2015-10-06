6bd0aaa951c40015d6437e7c667561785e04c069
Jeffrey Horn <jeffh@generalassemb.ly>
2015-10-06 12:16:51 -0400

    Fix tests

--- spec/stack.spec.js
+++ spec/stack.spec.js
@@ -8,25 +8,31 @@ describe('Stack', function() {
     var empty = {};
 
     expect(stack).toEqual(jasmine.any(Stack));
-    expect(stack._storage).toBe(empty);
+    expect(stack._storage).toEqual(empty);
   });
 });
 
 describe('Stack.prototype.push()', function() {
   it('adds new values to the stack', function() {
     var stack = new Stack();
-    var pushed = {'foo': 'bar'};
+    var pushed = {
+      foo: 'bar'
+    };
 
     stack.push(pushed);
 
-    expect(stack._storage).toBe({pushed});
+    expect(stack._storage).toEqual({
+      0: pushed
+    });
   });
 
   it('returns the stack itself', function() {
     var stack = new Stack();
-    var pushed = {'foo': 'bar'};
+    var pushed = {
+      foo: 'bar'
+    };
 
-    expect(stack.push(pushed)).toBe(stack._storage);
+    expect(stack.push(pushed)).toEqual(stack);
   });
 });
 
@@ -38,7 +44,7 @@ describe('Stack.prototype.pop()', function() {
     stack.push(1);
     stack.pop();
 
-    expect(stack._storage).toBe(empty);
+    expect(stack._storage).toEqual(empty);
   });
 
   it('returns the value removed from the stack', function() {
@@ -54,5 +60,4 @@ describe('Stack.prototype.pop()', function() {
 
     expect(stack.pop()).toBe(undefined);
   });
-
 });
