25faf7faa895c7083af4908bf14bb934884d0831
Antony Donovan <antony.donovan@generalassemb.ly>
2016-07-14 13:22:10 -0400

    Update from node-template
    Refresh for cohort 013

--- spec/stack.spec.js
+++ spec/stack.spec.js
@@ -1,19 +1,36 @@
 'use strict';
 
+// Allow chai syntax like `expect(foo).to.be.ok;`
+// jshint -W030
+
+const chai = require('chai');
+const expect = chai.expect;
+
 const Stack = require('../lib/stack');
 
-describe('Stack', () => {
-  it('creates an empty stack', () => {
+describe('Stack', function () {
+  it('creates an empty stack', function () {
     const stack = new Stack();
     const empty = {};
 
-    expect(stack).toEqual(jasmine.any(Stack));
-    expect(stack._storage).toEqual(empty);
+    expect(stack).to.be.an.instanceof(Stack);
+    expect(stack._storage).to.deep.equal(empty);
+  });
+
+  xit('creates a stack containing the arguments', function () {
+    const stack = new Stack('a', 'b', 'c');
+    const storage = {
+      0: 'a',
+      1: 'b',
+      2: 'c',
+    };
+
+    expect(stack._storage).to.deep.equal(storage);
   });
 });
 
-describe('Stack.prototype.push()', () => {
-  it('adds new values to the stack', () => {
+describe('Stack.prototype.push()', function () {
+  it('adds new values to the stack', function () {
     const stack = new Stack();
     const pushed = {
       foo: 'bar',
@@ -21,43 +38,43 @@ describe('Stack.prototype.push()', () => {
 
     stack.push(pushed);
 
-    expect(stack._storage).toEqual({
+    expect(stack._storage).to.deep.equal({
       0: pushed,
     });
   });
 
-  it('returns the stack itself', () => {
+  xit('returns the stack itself', function () {
     const stack = new Stack();
     const pushed = {
       foo: 'bar',
     };
 
-    expect(stack.push(pushed)).toEqual(stack);
+    expect(stack.push(pushed)).to.deep.equal(stack);
   });
 });
 
-describe('Stack.prototype.pop()', () => {
-  it('removes the most recently added value from the stack', () => {
+describe('Stack.prototype.pop()', function () {
+  it('removes the most recently added value from the stack', function () {
     const stack = new Stack();
     const empty = {};
 
     stack.push(1);
     stack.pop();
 
-    expect(stack._storage).toEqual(empty);
+    expect(stack._storage).to.deep.equal(empty);
   });
 
-  it('returns the value removed from the stack', () => {
+  it('returns the value removed from the stack', function () {
     const stack = new Stack();
 
     stack.push(1);
 
-    expect(stack.pop()).toBe(1);
+    expect(stack.pop()).to.equal(1);
   });
 
-  it('returns undefined when called on an empty stack', () => {
+  it('returns undefined when called on an empty stack', function () {
     const stack = new Stack();
 
-    expect(stack.pop()).toBe(undefined);
+    expect(stack.pop()).to.be.undefined;
   });
 });
